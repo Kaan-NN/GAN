959027cc7385857c3884d13672725fe4162ae569
Kaan Koyuncu <kaan.d.koyuncu@gmail.com>
2022-10-04 19:45:06 +0200

    fc + activation works. currently solves XOR

--- GAN.py
+++ GAN.py
@@ -1,30 +1,45 @@
+from threading import activeCount
 from NN.CORE.layers import activation, avgPool, fullyConnected, maxPool, pooling
 from NN.CORE.networks import network
+from NN.UTIL.data import preprocess
 from NN.UTIL.profiling import profile
+from NN.UTIL.activation import tanh, tanhDerivative
+from NN.UTIL.loss import mse, mseDerivative
 import numpy as np
 import os
 from PIL import Image
-from NN.UTIL.data import preprocess
 
 @profile
 def main():
-    rawData = "C:/Users/Kaan/Desktop/KI-Neural Network/GAN/DATA/data"
-    processedData = "C:/Users/Kaan/Desktop/KI-Neural Network/GAN/DATA/processed"
-    preprocess(rawData, processedData)
-    test = np.empty(shape=(len(os.listdir(processedData)), 3, 1000, 1000))
-    i = 0
-    for file in os.listdir(processedData):
-        test[i, :, :, :] = np.load(f"{processedData}\{file}")
-        i = i+1
+    #rawData = "C:/Users/Kaan/Desktop/KI-Neural Network/GAN/DATA/data"
+    #processedData = "C:/Users/Kaan/Desktop/KI-Neural Network/GAN/DATA/processed"
+    #preprocess(rawData, processedData)
+    #test = np.empty(shape=(len(os.listdir(processedData)), 3, 1000, 1000))
+    #i = 0
+    #for file in os.listdir(processedData):
+    #    test[i, :, :, :] = np.load(f"{processedData}\{file}")
+    #    i = i+1
     GAN = network()
-    GAN.addLayer(maxPool(test, 4, 4))
-    i = 0
-    for image in GAN.layers[0].forwardPropagation():
+    trainingData = np.array([[[0,0]], [[0,1]], [[1,0]], [[1,1]]])
+    dataLabel = np.array([[[0]], [[1]], [[1]], [[0]]])
+
+    GAN.addLayer(fullyConnected(2, 3))
+    GAN.addLayer(activation(tanh, tanhDerivative))
+    GAN.addLayer(fullyConnected(3, 1))
+    GAN.addLayer(activation(tanh, tanhDerivative))
+
+    GAN.useLoss(mse, mseDerivative)
+
+    GAN.train(trainingData, dataLabel, 5000, .1)
+
+    print(GAN.predict(trainingData))
+    #i = 0
+    #for image in GAN.layers[0].forwardPropagation():
     #for image in test:
-        image = image.result().astype(np.uint8)
-        im = Image.fromarray(image.transpose(1,2,0).astype(np.uint8))
-        im.save(f"your_file{i}.jpeg")
-        i = i+1
+    #    image = image.result().astype(np.uint8)
+    #    im = Image.fromarray(image.transpose(1,2,0).astype(np.uint8))
+    #    im.save(f"your_file{i}.jpeg")
+    #    i = i+1
 
 if __name__ == "__main__":
     main()
--- NN/CORE/networks.py
+++ NN/CORE/networks.py
@@ -1,4 +1,4 @@
-from pathlib import Path
+from random import sample
 
 class network:
 
@@ -28,3 +28,27 @@ class network:
         for layer in reversed(self.layers):
             error = layer.backwardPropagation(error, learningrate)
         return error
+
+    def train(self, trainingData, solution, epochs, learningrate):
+        samples = len(trainingData)
+        for i in range(epochs):
+            errorDisplay = 0
+            for j in range(samples):
+                output = self.forward(trainingData[j])
+
+                errorDisplay += self.loss(solution[j], output)
+
+                error = self.lossDerivative(solution[j], output)
+                for layer in reversed(self.layers):
+                    error = layer.backwardPropagation(error, learningrate)
+
+            errorDisplay /= samples
+            print(f"training cycle {i} / {epochs}   error={errorDisplay}")
+
+    def predict(self, inputData):
+        result = []
+        for sample in inputData:
+            output = sample
+            result.append(self.forward(output))
+
+        return result
